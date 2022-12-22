002ca618effd923c6be6c6736b5da2c73a11088b
Ilia Korepanov <ikorepanov@gmail.com>
2022-12-22 22:22:33 +0700

    Add templates

--- erm/erm/settings.py
+++ erm/erm/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'erm.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- erm/responds/views.py
+++ erm/responds/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def index(request):
-    html = '<h1> Какой-то заголовок </h1>'
-    return HttpResponse(html)
+    template = 'responds/index.html'
+    return render(request, template)
