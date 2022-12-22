028da5445355c164d42907233a1bd3a8624c3e64
Ilia Korepanov <ikorepanov@gmail.com>
2022-12-22 18:02:40 +0700

    Modify urls.py, add main urls.py, add index view

--- erm/responds/views.py
+++ erm/responds/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    html = '<h1> Какой-то заголовок </h1>'
+    return HttpResponse(html)
