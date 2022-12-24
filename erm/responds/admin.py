a5b3561814ed120e67bf7afc61a4b4d0dee09db0
Ilia Korepanov <ikorepanov@gmail.com>
2022-12-24 21:07:55 +0700

    Add models, register in admin.py

--- erm/responds/admin.py
+++ erm/responds/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Respond, Company, Position, CV, Template
+
+
+class RespondAdmin(admin.ModelAdmin):
+    list_display = (
+        'resp_date',
+        'company',
+        'position',
+        'cv',
+        'template',
+    )
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+    )
+
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+    )
+
+
+class CVAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+    )
+
+
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+    )
+
+
+admin.site.register(Respond, RespondAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Position, PositionAdmin)
+admin.site.register(CV, CVAdmin)
+admin.site.register(Template, TemplateAdmin)
--- erm/responds/models.py
+++ erm/responds/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class CV(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Template(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Respond(models.Model):
+    resp_date = models.DateField()
+    company = models.ForeignKey(
+        Company,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='responds',
+    )
+    position = models.ForeignKey(
+        Position,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='responds',
+    )
+    cv = models.ForeignKey(
+        CV,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='responds',
+    )
+    template = models.ForeignKey(
+        Template,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='responds',
+    )
