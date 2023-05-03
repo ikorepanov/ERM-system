cb612fac573ee60460004753db865c5d32d9efe9
Ilia Korepanov <ikorepanov@gmail.com>
2023-05-03 16:49:05 +0700

    Complete authentication system

--- erm/users/views.py
+++ erm/users/views.py
@@ -5,5 +5,5 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy('posts:index')
+    success_url = reverse_lazy('responses:index')
     template_name = 'users/signup.html'
