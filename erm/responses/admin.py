6e21f44c1ad0f45b60810c5c6880b7d3b9ca4d52
Ilia Korepanov <ikorepanov@gmail.com>
2023-12-09 18:30:16 +0700

    Register JobApplication in admin

--- erm/responses/admin.py
+++ erm/responses/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
-from .models import (Employer, EmployerContact, Position, Response,
-                     ThirdPartyCompany, ThirdPartyContact)
+from .models import (Employer, EmployerContact, JobApplication, Position,
+                     Response, ThirdPartyCompany, ThirdPartyContact)
+
+
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'date_applied',
+        'company_name',
+        'resume',
+        'cover_letter',
+    )
+    search_fields = ('company_name',)
+    list_filter = ('date_applied',)
 
 
 class EmployerAdmin(admin.ModelAdmin):
@@ -71,3 +83,4 @@ admin.site.register(EmployerContact, EmployerContactAdmin)
 admin.site.register(ThirdPartyCompany, ThirdPartyCompanyAdmin)
 admin.site.register(ThirdPartyContact, ThirdPartyContactAdmin)
 admin.site.register(Response, ResponseAdmin)
+admin.site.register(JobApplication, JobApplicationAdmin)
