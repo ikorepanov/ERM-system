697d6aa8ef0d87f26607b93a042556714011d45c
Ilia Korepanov <ikorepanov@gmail.com>
2023-12-21 12:13:48 +0700

    Add unique constraints

--- erm/responses/models.py
+++ erm/responses/models.py
@@ -13,6 +13,7 @@ class Employer(models.Model):
         help_text='Укажите название компании-работодателя',
     )
     website = models.URLField(
+        unique=True,
         blank=True,
         null=True,
         verbose_name='Сайт',
@@ -20,7 +21,7 @@ class Employer(models.Model):
     )
 
     class Meta:
-        ordering = ('name',)
+        ordering = ['name']
         verbose_name = 'Работодатель'
         verbose_name_plural = 'Работодатели'
 
@@ -37,6 +38,7 @@ class Agency(models.Model):
         help_text='Укажите название кадрового агентства',
     )
     website = models.URLField(
+        unique=True,
         blank=True,
         null=True,
         verbose_name='Сайт',
@@ -44,7 +46,7 @@ class Agency(models.Model):
     )
 
     class Meta:
-        ordering = ('name',)
+        ordering = ['name']
         verbose_name = 'Агентство'
         verbose_name_plural = 'Агентства'
 
@@ -117,7 +119,27 @@ class Contact(models.Model):
     )
 
     class Meta:
-        ordering = ('last_name', 'first_name',)
+        ordering = ['last_name', 'first_name']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['first_name',
+                        'last_name',
+                        'phone'],
+                name='unique_person_phone',
+            ),
+            models.UniqueConstraint(
+                fields=['first_name',
+                        'last_name',
+                        'email'],
+                name='unique_person_email',
+            ),
+            models.UniqueConstraint(
+                fields=['first_name',
+                        'last_name',
+                        'telegram'],
+                name='unique_person_telegram',
+            )
+        ]
         verbose_name = 'Контакт'
         verbose_name_plural = 'Контакты'
 
@@ -218,190 +240,3 @@ class Response(models.Model):
         if self.agency:
             return f'{self.position} в {self.employer} через {self.agency}'
         return f'{self.position} в {self.employer}'
-
-
-# class JobApplication(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     date_applied = models.DateField()
-#     company_name = models.CharField(max_length=255)
-#     resume = models.FileField(upload_to='resumes/')
-#     cover_letter = models.TextField()
-
-#     def __str__(self):
-#         return self.company_name
-
-
-# class Employer(models.Model):
-#     name = models.CharField(
-#         max_length=100,
-#         verbose_name='Employer Name'
-#     )
-#     website = models.URLField(
-#         null=True,  # поле может быть пустым в БД
-#         blank=True,  # поле может быть пустым в форме
-#         verbose_name='Employer Website',
-#         help_text='Enter the employer website URL'
-#     )
-
-#     def __str__(self):
-#         return self.name
-
-
-# class Position(models.Model):
-#     position = models.CharField(
-#         max_length=100,
-#         verbose_name='Position Name'
-#     )
-
-#     def __str__(self):
-#         return self.position
-
-
-# class EmployerContact(models.Model):
-#     first_name = models.CharField(
-#         max_length=100,
-#         verbose_name='First Name'
-#     )
-#     last_name = models.CharField(
-#         max_length=100,
-#         verbose_name='Last Name'
-#     )
-#     role = models.CharField(
-#         max_length=100,
-#         verbose_name='Role (Position)'
-#     )
-#     email = models.EmailField(
-#         null=True,
-#         blank=True,
-#         verbose_name='Email'
-#     )
-#     linkedin = models.CharField(
-#         max_length=100,
-#         null=True,
-#         blank=True,
-#         verbose_name='LinkedIn Account'
-#     )
-#     phone = models.CharField(
-#         max_length=100,
-#         null=True,
-#         blank=True,
-#         verbose_name='Phone'
-#     )
-#     company = models.ForeignKey(
-#         Employer,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         related_name='employer_contacts',
-#         verbose_name='Employer'
-#     )
-
-#     def __str__(self):
-#         return f'{self.first_name} {self.last_name}'
-
-
-# class ThirdPartyCompany(models.Model):
-#     name = models.CharField(
-#         max_length=100,
-#         verbose_name='Company Name'
-#     )
-#     website = models.URLField(
-#         null=True,
-#         blank=True,
-#         verbose_name='Company Website',
-#         help_text='Enter the third-party company website URL'
-#     )
-
-#     def __str__(self):
-#         return self.name
-
-
-# class ThirdPartyContact(models.Model):
-#     first_name = models.CharField(
-#         max_length=100,
-#         verbose_name='First Name'
-#     )
-#     last_name = models.CharField(
-#         max_length=100,
-#         verbose_name='Last Name'
-#     )
-#     role = models.CharField(
-#         max_length=100,
-#         verbose_name='Role (Position)'
-#     )
-#     email = models.EmailField(
-#         null=True,
-#         blank=True,
-#         verbose_name='Email'
-#     )
-#     linkedin = models.CharField(
-#         max_length=100,
-#         null=True,
-#         blank=True,
-#         verbose_name='LinkedIn Account'
-#     )
-#     phone = models.CharField(
-#         max_length=100,
-#         null=True,
-#         blank=True,
-#         verbose_name='Phone'
-#     )
-#     company = models.ForeignKey(
-#         ThirdPartyCompany,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         blank=True,
-#         related_name='thirdpartycontact_contacts',
-#         verbose_name='Third-Party Company'
-#     )
-
-#     def __str__(self):
-#         return f'{self.first_name} {self.last_name}'
-
-
-# class Response(models.Model):
-#     user = models.ForeignKey(
-#         User,
-#         on_delete=models.CASCADE,
-#         related_name='user_responses',
-#         verbose_name='User'
-#     )
-#     date = models.DateField(
-#         auto_now_add=True,
-#         verbose_name='Response Date'
-#     )
-#     employer = models.ForeignKey(
-#         Employer,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         related_name='employer_responses',
-#         verbose_name='Employer'
-#     )
-#     position = models.ForeignKey(
-#         Position,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         related_name='position_responses',
-#         verbose_name='Position'
-#     )
-#     employercontact = models.ForeignKey(
-#         EmployerContact,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         related_name='employercontact_responses',
-#         verbose_name='Employer Contact'
-#     )
-#     thirdpartycontact = models.ForeignKey(
-#         ThirdPartyContact,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         related_name='thirdpartycontact_responses',
-#         verbose_name='Third-Party Contact'
-#     )
-#     status = models.CharField(
-#         max_length=100,
-#         verbose_name='Response Status',
-#         help_text='Enter or update response status'
-#     )
-
-#     def __str__(self):
-#         return f'{self.position}, {self.employer}'
